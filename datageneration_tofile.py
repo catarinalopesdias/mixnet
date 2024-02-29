2ccf5bf13d56fbe696045ee7d2562302f6dea550
catarinalopesdias <catlopesdias@gmail.com>
2024-02-29 10:59:22 +0100

    last updates

--- datageneration_tofile.py
+++ datageneration_tofile.py
@@ -1,4 +1,4 @@
- #!/usr/bin/env python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Tue Feb 13 08:56:39 2024
@@ -8,17 +8,19 @@ and then transforms to phase, and then adds background and artifacts. it saves p
 @author: catarinalopesdias
 """
 
+import h5py
 import numpy as np
 from visualize_volumes import view_slices_3dNew
 from create_datasetfunctions import simulate_susceptibility_sources, forward_convolution
-from functionsfromsteffen import apply_random_brain_mask #,  calc_gauss_function_np#, apply_random_brain_mask, distance_to_plane_np, distance_to_plane
+# ,  calc_gauss_function_np#, apply_random_brain_mask, distance_to_plane_np, distance_to_plane
+from functionsfromsteffen import apply_random_brain_mask
 from generate_backgroundfield_steffen_function import add_z_gradient
 from boundaryeffects_function import add_boundary_artifacts
 
 import tensorflow as tf
 
-num_train_instances = 50
-size = 128    #[128,128,128]
+num_train_instances = 200
+size = 128  # [128,128,128]
 rect_num = 200
 
 # Parameters background
@@ -40,15 +42,15 @@ wrap_input_data = True
 ##############################################################################
 # create dipole kernel (cuboid size )
 ##############################################################################
-shape_of_sus_cuboid = [size,size, size] # of the susceptibilitz sources
+shape_of_sus_cuboid = [size, size, size]  # of the susceptibilitz sources
 
 ###############################################################################
 # Create synthetic dataset
 ###############################################################################
 # Inizialize 3 steps: gt, phase, background+phase
 # 4D-cuboids (training samples, size, size, size)
-sim_gt =    np.zeros((num_train_instances,size,size,size))
-sim_fwgt =  np.zeros((num_train_instances,size,size,size)) 
+sim_gt = np.zeros((num_train_instances, size, size, size))
+sim_fwgt = np.zeros((num_train_instances, size, size, size))
 mask = np.ones_like(sim_gt)
 sim_fwgt_mask = np.zeros_like(sim_gt)
 bgf = np.zeros_like(mask)
@@ -56,10 +58,10 @@ sim_fwgt_mask_bg = np.zeros_like(sim_gt)
 sim_fwgt_mask_bg_sn = np.zeros_like(mask)
 sim_fwgt_mask_bg_sn_wrapped = np.zeros_like(mask)
 bgf_mask = np.zeros_like(mask)
-sensornoise =  np.zeros_like(mask)
+sensornoise = np.zeros_like(mask)
 
 Xinput = np.ones_like(sim_gt)
-Xongoing =  np.ones_like(sim_gt)
+Xongoing = np.ones_like(sim_gt)
 
 #################################################################
 ################################################################
@@ -70,145 +72,181 @@ print("iterate epochs,sim susc,  convolve, add background noise")
 
 for epoch_i in range(num_train_instances):
     print("epoch ", str(epoch_i))
-    
-    # Create ground thruth - add rectangles               
-    sim_gt[epoch_i,:,:,:] = simulate_susceptibility_sources(simulation_dim = size, rectangles_total = rect_num, plot=False)
-    # Phase:forward convolution with the dipole kernel 
-    sim_fwgt[epoch_i,:,:,:]  = forward_convolution(sim_gt[epoch_i,:,:,:])
+
+    # Create ground thruth - add rectangles
+    sim_gt[epoch_i, :, :, :] = simulate_susceptibility_sources(
+        simulation_dim=size, rectangles_total=rect_num, plot=False)
+    # Phase:forward convolution with the dipole kernel
+    sim_fwgt[epoch_i, :, :, :] = forward_convolution(sim_gt[epoch_i, :, :, :])
 
  ########################################################################################################
  ########################################################################################################
  ########################################################################################################
 
-    Xinput[epoch_i,:,:,:] = sim_fwgt[epoch_i,:,:,:]
-    Xongoing[epoch_i,:,:,:] = sim_fwgt[epoch_i,:,:,:]
-    
+    Xinput[epoch_i, :, :, :] = sim_fwgt[epoch_i, :, :, :]
+    Xongoing[epoch_i, :, :, :] = sim_fwgt[epoch_i, :, :, :]
+
     if apply_masking:
 
-        sim_fwgt_mask[epoch_i,:,:,:], mask[epoch_i,:,:,:] = apply_random_brain_mask(sim_fwgt[epoch_i,:,:,:])
-        
-        Xinput[epoch_i,:,:,:] = sim_fwgt_mask[epoch_i,:,:,:]
-        
+        sim_fwgt_mask[epoch_i, :, :, :], mask[epoch_i, :, :,
+                                              :] = apply_random_brain_mask(sim_fwgt[epoch_i, :, :, :])
+
+        Xinput[epoch_i, :, :, :] = sim_fwgt_mask[epoch_i, :, :, :]
+
 ######################
     if backgroundfield:
 
-          #print("add z gradient")
-          bgf[epoch_i,:,:,:] = add_z_gradient(bgf[epoch_i,:,:,:], gradient_slope_range)
-          
+        #print("add z gradient")
+        bgf[epoch_i, :, :, :] = add_z_gradient(
+            bgf[epoch_i, :, :, :], gradient_slope_range)
 
+        if apply_masking:
 
-          if apply_masking:
+            bgf_mask[epoch_i, :, :, :] = add_boundary_artifacts(
+                bgf[epoch_i, :, :, :], mask[epoch_i,
+                                            :, :, :], boundary_artifacts_mean,
+                boundary_artifacts_std)
 
-              bgf_mask[epoch_i,:,:,:] = add_boundary_artifacts(
-                  bgf[epoch_i,:,:,:], mask[epoch_i,:,:,:], boundary_artifacts_mean,
-                  boundary_artifacts_std)
-          
-          
-          sim_fwgt_mask_bg[epoch_i,:,:,:] = tf.add(sim_fwgt_mask[epoch_i,:,:,:], bgf_mask[epoch_i,:,:,:]) #????????????dkjfsdfsdsdklfjdsjfhhsdgfkjsdfkllfdfjfk
+        # ????????????dkjfsdfsdsdklfjdsjfhhsdgfkjsdfkllfdfjfk
+        sim_fwgt_mask_bg[epoch_i, :, :, :] = tf.add(
+            sim_fwgt_mask[epoch_i, :, :, :], bgf_mask[epoch_i, :, :, :])
 
-          Xongoing[epoch_i,:,:,:] = tf.add(Xongoing[epoch_i,:,:,:], bgf_mask[epoch_i,:,:,:])
+        Xongoing[epoch_i, :, :, :] = tf.add(
+            Xongoing[epoch_i, :, :, :], bgf_mask[epoch_i, :, :, :])
 
-######################          
+######################
     if sensor_noise:
-        
+
         tf_noise = tf.random.normal(
-            shape=[128,128,128],#]Xongoing[epoch_i,:,:,:].get_shape(),
+            shape=[128, 128, 128],  # ]Xongoing[epoch_i,:,:,:].get_shape(),
             mean=sensor_noise_mean,
             stddev=sensor_noise_std,
             dtype=tf.float32)
-        
-        sim_fwgt_mask_bg_sn[epoch_i, :,:,:] = tf.add(sim_fwgt_mask_bg[epoch_i, :,:,:], tf_noise.numpy()) 
-        
-        Xongoing[epoch_i,:,:,:] = tf.add(Xongoing[epoch_i, :,:,:], tf_noise.numpy()) 
-        
-        sensornoise[epoch_i, :,:,:] = tf_noise.numpy()
-  ######################                
+
+        sim_fwgt_mask_bg_sn[epoch_i, :, :, :] = tf.add(
+            sim_fwgt_mask_bg[epoch_i, :, :, :], tf_noise.numpy())
+
+        Xongoing[epoch_i, :, :, :] = tf.add(
+            Xongoing[epoch_i, :, :, :], tf_noise.numpy())
+
+        sensornoise[epoch_i, :, :, :] = tf_noise.numpy()
+  ######################
     if wrap_input_data:
-         #print('wrap_data')
-         value_range = 2.0 * np.pi
-                  # shift from [-pi,pi] to [0,2*pi]
-         sim_fwgt_mask_bg_sn_wrapped[epoch_i, :,:,:] = tf.add(sim_fwgt_mask_bg_sn[epoch_i, :,:,:], value_range / 2.0)
-         
-         Xongoing[epoch_i, :,:,:] = tf.add(Xongoing[epoch_i, :,:,:], value_range / 2.0)
+        # print('wrap_data')
+        value_range = 2.0 * np.pi
+        # shift from [-pi,pi] to [0,2*pi]
+        sim_fwgt_mask_bg_sn_wrapped[epoch_i, :, :, :] = tf.add(
+            sim_fwgt_mask_bg_sn[epoch_i, :, :, :], value_range / 2.0)
 
+        Xongoing[epoch_i, :, :, :] = tf.add(
+            Xongoing[epoch_i, :, :, :], value_range / 2.0)
 
-          # # calculate wrap counts
-          # self.tensor['wrap_count'] = tf.floor(
-           #     tf.divide(X, value_range))
-              
-         sim_fwgt_mask_bg_sn_wrapped[epoch_i, :,:,:] = tf.math.floormod(sim_fwgt_mask_bg_sn_wrapped[epoch_i, :,:,:], value_range)
-         Xongoing[epoch_i, :,:,:] = tf.math.floormod(Xongoing[epoch_i, :,:,:], value_range)
-     
-         
-                  # shift back to [-pi,pi]
-         sim_fwgt_mask_bg_sn_wrapped[epoch_i, :,:,:] = tf.subtract(sim_fwgt_mask_bg_sn_wrapped[epoch_i, :,:,:], value_range / 2.0)
-         
-         Xongoing[epoch_i, :,:,:] = tf.subtract(Xongoing[epoch_i, :,:,:], value_range / 2.0)
-           
- ######################          
+        # # calculate wrap counts
+        # self.tensor['wrap_count'] = tf.floor(
+        #     tf.divide(X, value_range))
+
+        sim_fwgt_mask_bg_sn_wrapped[epoch_i, :, :, :] = tf.math.floormod(
+            sim_fwgt_mask_bg_sn_wrapped[epoch_i, :, :, :], value_range)
+        Xongoing[epoch_i, :, :, :] = tf.math.floormod(
+            Xongoing[epoch_i, :, :, :], value_range)
+
+        # shift back to [-pi,pi]
+        sim_fwgt_mask_bg_sn_wrapped[epoch_i, :, :, :] = tf.subtract(
+            sim_fwgt_mask_bg_sn_wrapped[epoch_i, :, :, :], value_range / 2.0)
+
+        Xongoing[epoch_i, :, :, :] = tf.subtract(
+            Xongoing[epoch_i, :, :, :], value_range / 2.0)
+
+ ######################
     if apply_masking:
-              
-        #sim_gt_mask_bg_sn_wrapped = tf.multiply(tf.to_float(mask), sim_gt_mask_bg_sn_wrapped)
-        
-        #sim_gt_mask_bg_sn_wrapped = tf.multiply(tf.cast(mask. tf.float32), sim_gt_mask_bg_sn_wrapped)
-        sim_fwgt_mask_bg_sn_wrapped[epoch_i, :,:,:] = np.multiply(mask[epoch_i, :,:,:], sim_fwgt_mask_bg_sn_wrapped[epoch_i, :,:,:])
 
-        Xongoing[epoch_i, :,:,:] = np.multiply(mask[epoch_i, :,:,:], Xongoing[epoch_i, :,:,:])
+        #sim_gt_mask_bg_sn_wrapped = tf.multiply(tf.to_float(mask), sim_gt_mask_bg_sn_wrapped)
+
+        #sim_gt_mask_bg_sn_wrapped = tf.multiply(tf.cast(mask. tf.float32), sim_gt_mask_bg_sn_wrapped)
+        sim_fwgt_mask_bg_sn_wrapped[epoch_i, :, :, :] = np.multiply(
+            mask[epoch_i, :, :, :], sim_fwgt_mask_bg_sn_wrapped[epoch_i, :, :, :])
+
+        Xongoing[epoch_i, :, :, :] = np.multiply(
+            mask[epoch_i, :, :, :], Xongoing[epoch_i, :, :, :])
 
         #Xongoing = tf.multiply(mask[epoch_i, :,:,:], Xongoing[epoch_i, :,:,:])
 
-         #     Y = tf.multiply(tf.to_float(mask), Y)                  
-                    
-
-
+        #     Y = tf.multiply(tf.to_float(mask), Y)
 
 
 ##############################################################################
 # visualize
 ##############################################################################
 index = 3
-view_slices_3dNew(sim_gt[index,:,:,:], 50,50,50, vmin=-1.5, vmax=1.5, title="gt")
-view_slices_3dNew(sim_fwgt[index,:,:,:], 50,50,50, vmin=-1.5, vmax=1.5, title="fw")
+view_slices_3dNew(sim_gt[index, :, :, :], 50, 50, 50,
+                  vmin=-1.5, vmax=1.5, title="gt")
+view_slices_3dNew(sim_fwgt[index, :, :, :], 50, 50,
+                  50, vmin=-1.5, vmax=1.5, title="fw")
 
 
-view_slices_3dNew(Xinput[index,:,:,:], 50,50,50, vmin=-1.5, vmax=1.5, title="Xinput (fw+mask)")
-view_slices_3dNew(sim_fwgt_mask[index,:,:,:], 50,50,50, vmin=-0.5, vmax=0.5, title="fw+brain mask")
-view_slices_3dNew(mask[index,:,:,:], 50,50,50, vmin=-0.5, vmax=0.5, title="brain mask")
+view_slices_3dNew(Xinput[index, :, :, :], 50, 50, 50,
+                  vmin=-1.5, vmax=1.5, title="Xinput (fw+mask)")
+view_slices_3dNew(sim_fwgt_mask[index, :, :, :], 50,
+                  50, 50, vmin=-0.5, vmax=0.5, title="fw+brain mask")
+view_slices_3dNew(mask[index, :, :, :], 50, 50, 50,
+                  vmin=-0.5, vmax=0.5, title="brain mask")
 
 
-view_slices_3dNew(bgf[index,:,:,:],50,50,50, vmin=-10, vmax=10, title="background field")
-view_slices_3dNew(bgf_mask[index,:,:,:], 50, 50,50, vmin=-10, vmax=10, title="bg field + masking (bondary artifacts)")
-view_slices_3dNew(sim_fwgt_mask_bg[index,:,:,:], 50,50,50, vmin=-10, vmax=10, title="fw + mask +background")
-view_slices_3dNew(sim_fwgt_mask_bg[index,:,:,:], 50,50,50, vmin=-0.5, vmax=0.5, title="fw + mask +background+boundarz artifacs")
+view_slices_3dNew(bgf[index, :, :, :], 50, 50, 50,
+                  vmin=-10, vmax=10, title="background field")
+view_slices_3dNew(bgf_mask[index, :, :, :], 50, 50, 50, vmin=-10,
+                  vmax=10, title="bg field + masking (bondary artifacts)")
+view_slices_3dNew(sim_fwgt_mask_bg[index, :, :, :], 50,
+                  50, 50, vmin=-10, vmax=10, title="fw + mask +background")
+view_slices_3dNew(sim_fwgt_mask_bg[index, :, :, :], 50, 50, 50, vmin=-
+                  0.5, vmax=0.5, title="fw + mask +background+boundarz artifacs")
 
 
+view_slices_3dNew(sensornoise[index, :, :, :], 50,
+                  50, 50, vmin=-0.5, vmax=0.5, title="sn")
 
-view_slices_3dNew(sensornoise[index,:,:,:], 50, 50,50, vmin=-0.5, vmax=0.5, title="sn")
-
-view_slices_3dNew(sim_fwgt_mask_bg_sn[index,:,:,:], 50, 50,50, vmin=-10, vmax=10, title="fw+bg + mask (bondary artifacts)+sn")
-
-
-view_slices_3dNew(sim_fwgt_mask_bg_sn_wrapped[index,:,:,:], 50, 50,50, vmin=-10, vmax=10, title="bg field + masking (bondary artifacts)+wrapped")
+view_slices_3dNew(sim_fwgt_mask_bg_sn[index, :, :, :], 50, 50, 50,
+                  vmin=-10, vmax=10, title="fw+bg + mask (bondary artifacts)+sn")
 
 
-view_slices_3dNew(Xongoing[index,:,:,:], 50, 50,50, vmin=-10, vmax=10, title="X ongoing-fw,masj,bg,sn,wrapped")
+view_slices_3dNew(sim_fwgt_mask_bg_sn_wrapped[index, :, :, :], 50, 50, 50,
+                  vmin=-10, vmax=10, title="bg field + masking (bondary artifacts)+wrapped")
 
 
-titleGT = "syntheticdata/" + str(num_train_instances)+"GT.npy"
-np.save(titleGT,sim_gt)
+view_slices_3dNew(Xongoing[index, :, :, :], 50, 50, 50,
+                  vmin=-10, vmax=10, title="X ongoing-fw,masj,bg,sn,wrapped")
+############################
 
-titlephasebg = "syntheticdata/" + str(num_train_instances)+"phase_bg.npy"
 
-np.save(titlephasebg, Xongoing)
-titlephase = "syntheticdata/" + str(num_train_instances)+"phase.npy"
+############################
+# gt
+#titleGT = "datasynthetic/" + str(num_train_instances)+"GT.npy"
 
-np.save(titlephase, Xinput)
+#np.save(titleGT, sim_gt)
+##################################
+# PHASE BG
 
-#loaded_array = np.load('phase_bg.npy')
-          
-#view_slices_3dNew(loaded_array[index,:,:,:], 50, 50,50, vmin=-10, vmax=10, title="X ongoing-fw,masj,bg,sn,wrapped")
+#titlephasebg = "datasynthetic/" + str(num_train_instances)+"phase_bg.npy"
+#final = "/mnt/neuro/physics/catarina/" + titlephasebg
+final1 = "datasynthetic/" + str(num_train_instances)+"phase_bg" + ".h5"
 
-        
-          
-          
- 
+#h5f = h5py.File(final1, 'w')
+#h5f.create_dataset('dataset_1', data=Xongoing)
+# h5f.close()
+
+#np.save(titlephasebg, Xongoing)
+#np.save(final, Xongoing)
+#########################################################################
+#phase ###############
+
+#titlephase = "datasynthetic/" + str(num_train_instances)+"phase.npy"
+
+#final = "/mnt/neuro/physics/catarina/" + titlephase
+
+#np.save(titlephase, Xinput)
+#np.save(titlephase, Xinput)
+title = "datasynthetic/" + str(num_train_instances) + "samples"
+
+np.savez_compressed(title, sim_gt1 = sim_gt, phase_bg1 = Xongoing, phase1 = Xinput )
+###
+#loaded = np.load('')
--- load_existing_bg_model_testsynthetic.py
+++ load_existing_bg_model_testsynthetic.py
@@ -33,18 +33,28 @@ from tensorflow import keras
 # model data
 
 
-num_train_instances = 100 
-dataset_iterations = 300
-finalcheckpoint = 240
+num_train_instances = 130 
+dataset_iterations = 2000
+finalcheckpoint = 1977
 batch_size = 1
-gaaccumsteps = 12;
+gaaccumsteps = 10
+num_filter = 16
 lossU = "mse" # "mean_absolute_error" #"mse"# "mean_absolute_error" #"mse"    #mse
+#n#ewadam16cp-1977_trainsamples130_datasetiter2000_batchsize1_gaaccum10_loss_mse.ckpt
+#newadam64cp-0997_trainsamples100_datasetiter999_batchsize1_gaaccum10_loss_mse.ckpt
+#32cp-0416_trainsamples100_datasetiter999_batchsize1_gaaccum10_loss_mse.ckpt
+#32cp-0469_trainsamples100_datasetiter999_batchsize1_gaaccum10_loss_mse.ckpt
+#cp-0326_trainsamples100_datasetiter999_batchsize1_gaaccum10_loss_mse.ckpt
+#cp-0300_trainsamples100_datasetiter300_batchsize1_gaaccum10_loss_mse.ckpt.index
+#path = "checkpoints/bgremovalmodel/cp-0"+ str(finalcheckpoint) +"_trainsamples" + str(num_train_instances) + "_datasetiter" + str(dataset_iterations) + "_batchsize" + str(batch_size)+ "_gaaccum" + str(gaaccumsteps) + "_loss_" + lossU+".ckpt.index"
+path = "checkpoints/bgremovalmodel/newadam"+str(num_filter)+"cp-"+ str(finalcheckpoint) +"_trainsamples" + str(num_train_instances) + "_datasetiter" + str(dataset_iterations) + "_batchsize" + str(batch_size)+ "_gaaccum" + str(gaaccumsteps) + "_loss_" + lossU+".ckpt"
 
-
-path = "checkpoints/bgremovalmodel/cp-0"+ str(finalcheckpoint) +"_trainsamples" + str(num_train_instances) + "_datasetiter" + str(dataset_iterations) + "_batchsize" + str(batch_size)+ "_gaaccum" + str(gaaccumsteps) + "_loss_" + lossU+".ckpt"
 #path = "checkpoints/GAcp-0"+ str(epochs_train) + str(num_train)+ "trainsamples_" + str(epochs_train) +"epochs_" + "batchsize"+ str(batch_size)+ "_"+ str(gaaccumsteps) +"gaaccum" + "loss"+str(lossU)+".ckpt"
 #path = "checkpoints/GAcp-00"+str(num_epochs)+".ckpt/"
+
+#path = "models/backgroundremovalBOLLMAN/modelBR_trainsamples100_datasetiter300_batchsize1_gaaccum10_loss_mse.keras"
 model = tf.keras.models.load_model(path)
+#model = tf.keras.saving.load_model(path)
 
 
 model.compile(loss = lossU, optimizer = 'adam')
@@ -55,13 +65,29 @@ model.compile(loss = lossU, optimizer = 'adam')
 #################################################################
 
 # load unseen data 
+################################################
+#   Import data
+################################################
+#view input
+#view output
 
+#phase_bg = np.load('syntheticdata/phase_bg100.npy')
+#phase = np.load('syntheticdata/phase100.npy')
+######
+# compressed data
+#loaded = np.load('datasynthetic/130samples.npz')
+
+#phase = loaded['phase1']
+#phase_bg = loaded['phase_bg1']
+#del loaded
 #new data
-phasebg  = np.load('syntheticdata/50phase_bg.npy')
-phase  = np.load('syntheticdata/50phase.npy')
+phasebg  = np.load('datasynthetic/50phase_bg.npy')
+phase  = np.load('datasynthetic/50phase.npy')
+#phasebg  = np.load('datasynthetic/phase_bg100.npy')
+#phase  = np.load('datasynthetic/phase100.npy')
 num_instance = 50
           
-#view_slices_3dNew(loaded_array[index,:,:,:], 50, 50,50, vmin=-10, vmax=10, title="X ongoing-fw,masj,bg,sn,wrapped")
+
 
 
 
@@ -76,7 +102,7 @@ for epoch_i in range(3): #num_instance
 
     print(epoch_i)
     title =   "trained network with testing data 50 epochs: " + str(epoch_i)+ " " + lossU
-    pathi = "models/backgroundremoval/results/" + "trainsamples" + str(num_train_instances) + "_datasetiter" + str(dataset_iterations) + "_batchsize" + str(batch_size)+ "_gaaccum" + str(gaaccumsteps) + "_loss_" + lossU+ "_testset_epoch" + str(epoch_i)
+    pathi = "models/backgroundremovalBOLLMAN/results/train"+str(num_filter) + "trainsamples" + str(num_train_instances) + "_datasetiter" + str(dataset_iterations) + "_batchsize" + str(batch_size)+ "_gaaccum" + str(gaaccumsteps) + "_loss_" + lossU+ "_testset_epoch" + str(epoch_i)
     predicted, reference,error = visualize_all4(phasebg[epoch_i,:,:,:], phase[epoch_i,:,:,:], y_pred[0,:,:,:,0] ,title = title , save = True, path = pathi )
 #########################
 
